317c70fe468bde7f6fdf9941587a4f1f8164b9bf
richman_stewart <kiwipxl@gmail.com>
2021-01-03 20:20:26 +1100

    added create new label button

---
+++ client/src/components/label/CreateLabel.js
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { AiOutlinePlus } from "react-icons/ai";
+import { ImCross } from "react-icons/im";
+import { BsCheck } from "react-icons/bs";
+import Icon from "../Icon";
+import Input from "../input/Input";
+import { createLabel } from "../../redux/actions";
+
+const CreateLabel = ({ className }) => {
+  const inputRef = React.createRef();
+  const [selected, setSelected] = React.useState(false);
+  const dispatch = useDispatch();
+  const [name, setName] = React.useState("");
+
+  React.useEffect(() => {
+    if (selected) {
+      inputRef.current.focus();
+    }
+  }, [selected]);
+
+  if (selected) {
+    return (
+      <div className={className}>
+        <StyledIcon
+          Component={ImCross}
+          size={22}
+          variant="button"
+          onClick={() => {
+            setSelected(false);
+            setName("");
+          }}
+        ></StyledIcon>
+
+        <StyledInput
+          ref={inputRef}
+          value={name}
+          placeholder="Create new label"
+          onChange={(e) => setName(e.target.value)}
+        ></StyledInput>
+
+        <StyledIcon
+          variant="button"
+          Component={BsCheck}
+          size={22}
+          onClick={() => {
+            dispatch(createLabel(null, name));
+            setSelected(false);
+            setName("");
+          }}
+        ></StyledIcon>
+      </div>
+    );
+  } else {
+    return (
+      <div className={className} onClick={() => setSelected(true)}>
+        <StyledIcon
+          Component={AiOutlinePlus}
+          size={22}
+          onClick={() => setSelected(true)}
+        ></StyledIcon>
+
+        <Text>Create new label</Text>
+      </div>
+    );
+  }
+};
+
+const StyledIcon = styled(Icon)`
+  flex: 0.1;
+`;
+
+const Text = styled.span`
+  margin-left: 10px;
+  flex: 1;
+  font-size: 15px;
+  opacity: ${(props) => props.theme.highEmphasisOpacity};
+`;
+
+const StyledInput = styled(Input)`
+  margin-left: 10px;
+  flex: 1;
+  font-size: 15px;
+  opacity: ${(props) => props.theme.highEmphasisOpacity};
+`;
+
+export default styled(CreateLabel)`
+  display: flex;
+  align-items: center;
+`;
--- client/src/screens/ManageLabelsScreen.js
+++ client/src/screens/ManageLabelsScreen.js
@@ -7,6 +7,7 @@ import List from "../components/nav/List";
 import ListRow from "../components/nav/ListRow";
 import { addNoteLabel, removeNoteLabel } from "../redux/actions";
 import EditableLabel from "../components/label/EditableLabel";
+import CreateLabel from "../components/label/CreateLabel";
 
 const ManageLabelsScreen = ({ className }) => {
   const labels = useSelector((state) => state.labels);
@@ -16,6 +17,10 @@ const ManageLabelsScreen = ({ className }) => {
       <Header backButton title="Edit Labels"></Header>
 
       <List>
+        <CreateLabelListRow>
+          <StyledCreateLabel></StyledCreateLabel>
+        </CreateLabelListRow>
+
         {Object.keys(labels).map((lid) => {
           const label = labels[lid];
 
@@ -29,6 +34,19 @@ const ManageLabelsScreen = ({ className }) => {
     </div>
   );
 };
+
+const CreateLabelListRow = styled(ListRow)`
+  border-width: 1px;
+  border-color: ${(props) => props.theme.elevate02dp(props.theme.surfaceColor)};
+  border-style: solid;
+`;
+
+const StyledCreateLabel = styled(CreateLabel)`
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
 
 const StyledEditableLabel = styled(EditableLabel)`
   width: calc(100% - 20px);
