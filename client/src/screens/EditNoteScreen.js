faed1913ec54bb72ea08423631396600120e9594
richman_stewart <kiwipxl@gmail.com>
2021-01-04 14:27:36 +1100

    remove rich text editor

--- client/src/screens/EditNoteScreen.js
+++ client/src/screens/EditNoteScreen.js
@@ -2,16 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
-import { BsFileRichtext } from "react-icons/bs";
+import { GoPencil } from "react-icons/go";
 import { IoMdAddCircle } from "react-icons/io";
 import { MdMoreVert } from "react-icons/md";
 import Header from "../components/Header";
 import Icon from "../components/Icon";
 import NoteEditor from "../components/note/editor/NoteEditor";
-import RichTextEditor from "../components/note/editor/RichTextEditor";
 
 const EditNoteScreen = ({ className }) => {
-  const [richTextEnabled, setRichTextEnabled] = React.useState(false);
   const routerHistory = useHistory();
   const { nid } = useParams();
   const note = useSelector((state) => state.notes[nid]);
@@ -27,44 +25,20 @@ const EditNoteScreen = ({ className }) => {
     return <div></div>;
   }
 
-  function toggleRichText() {
-    setRichTextEnabled(!richTextEnabled);
-  }
-
   return (
     <div className={className}>
       <Header backButton>
         <FlexSpace></FlexSpace>
 
-        <RichTextIcon
-          Component={BsFileRichtext}
-          variant="button"
-          size={28}
-          onClick={toggleRichText}
-          preventMouseDown
-        ></RichTextIcon>
-
         <AddLabelsIcon
-          Component={BsFileRichtext}
+          Component={GoPencil}
           variant="button"
           size={28}
           onClick={() => routerHistory.push(`/note/${nid}/labels`)}
         ></AddLabelsIcon>
       </Header>
 
-      <StyledNoteEditor
-        nid={nid}
-        richTextEnabled={richTextEnabled}
-        RichTextEditor={StyledRichTextEditor}
-      >
-        {(title, body) => (
-          <StyledRichTextEditor
-            editorState={body.editorState}
-            setEditorState={body.setEditorState}
-            enabled={richTextEnabled}
-          ></StyledRichTextEditor>
-        )}
-      </StyledNoteEditor>
+      <StyledNoteEditor nid={nid}></StyledNoteEditor>
 
       <Footer>
         <AddContentIcon
@@ -85,12 +59,6 @@ const FlexSpace = styled.div`
   flex: 1;
 `;
 
-const RichTextIcon = styled(Icon)`
-  margin: 5px;
-  margin-left: 10px;
-  margin-right: 10px;
-`;
-
 const AddLabelsIcon = styled(Icon)`
   margin: 5px;
   margin-left: 10px;
@@ -98,19 +66,9 @@ const AddLabelsIcon = styled(Icon)`
 `;
 
 const StyledNoteEditor = styled(NoteEditor)`
-  position: relative;
-  top: ${(props) => (props.richTextEnabled ? Header.height : 0)}px;
-  transition: top 0.4s, height 0.4s;
   overflow-y: auto;
   flex: 1;
   margin-top: -10px;
-`;
-
-const StyledRichTextEditor = styled(RichTextEditor)`
-  position: fixed;
-  top: ${(props) => (props.enabled ? Header.height + 10 : 0)}px;
-  transition: top 0.4s;
-  width: 350px;
 `;
 
 const Footer = styled.div`
